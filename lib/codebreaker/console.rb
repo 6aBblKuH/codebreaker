6e481ad848204fdd446e0c314a554cdda0a92ba9
Dmitriy Bavykin <1996@3g.ua>
2017-11-23 14:01:10 +0200

    difficultity lvls, hints and phrases were added
    	modified:   lib/codebreaker/console.rb
    	modified:   lib/codebreaker/game.rb
    	new file:   lib/codebreaker/src/phrases.yml

--- lib/codebreaker/console.rb
+++ lib/codebreaker/console.rb
@@ -1,4 +1,11 @@
+require "yaml"
+
 class Console
+  PhrasesPath = 'lib/codebreaker/src/phrases.yml'
+
+  def initialize
+    @phrases = YAML.load_file(PhrasesPath)
+  end
 
   def message(val)
     puts val
@@ -7,5 +14,23 @@ class Console
   def question(q)
     puts q
     gets.chomp
+  end
+
+  def rules
+    message(@phrases[:rules])
+  end
+
+  def difficulty_rules
+    @phrases[:difficulty].each do|diff, descr|
+      message("#{diff}: #{descr}")
+    end
+  end
+
+  def win
+    message @phrases[:win]
+  end
+
+  def loose(secret_code)
+    message @phrases[:loose] << secret_code.join
   end
 end
--- lib/codebreaker/game.rb
+++ lib/codebreaker/game.rb
@@ -1,42 +1,97 @@
 require_relative "console"
 require "pry"
+
 class Game
+
+  Difficulty = {
+    easy: {attempts: 30, hints: 3 },
+    medium: {attempts: 15, hints: 2 },
+    hard: {attempts: 10, hints: 1 }
+  }
 
   attr_accessor :secret_code
   def initialize
     @secret_code = []
-    @attempts_count = 20
     @console = Console.new
+    # welcome
+    new_game
+  end
+
+  def welcome
+    @console
+  end
+
+  def new_game
+    @console.rules
+    @console.difficulty_rules
+    choose_the_difficulty
+    secret_code
     game_round
   end
 
+  def choose_the_difficulty
+    answer = @console.question 'choose the difficulty lvl'
+    difficulty(answer)
+  end
+
+  def difficulty(diff)
+    lvl = Difficulty[diff.to_sym]
+    if lvl
+      setup_the_difficulty(lvl)
+    else
+       @console.message('Spelling failure')
+       choose_the_difficulty
+    end
+  end
+
+  def setup_the_difficulty(lvl)
+    @attempts_count = lvl[:attempts]
+    @hints_count = lvl[:hints]
+  end
+
   def game_round
-    @attempts_count -= 1
-    @user_answer = @console.question('Put the numbers')
+    @user_answer = @console.question("Put the numbers. You`ve got #{@attempts_count} attempts")
     handle_answer
   end
 
   def handle_answer
-    validate_answer
+    if @user_answer == 'hint'
+      hint
+    elsif validate_answer
+      @attempts_count -= 1
+      handle_code
+    end
+    game_round if @attempts_count > 0
+  end
+
+  def handle_code
     user_code = []
     @user_answer.each_char { |chr| user_code << chr.to_i }
     if @attempts_count > 0
-      compare_codes(user_code) ? win : game_round
+      @console.win if compare_codes(user_code)
     else
-      loose
+      @console.loose(@secret_code)
     end
   end
 
   def validate_answer
     @console.message 'Put only numbers' if /[^0-9]/ =~ @user_answer
     @console.message 'Put 4-digital number' if @user_answer.length != 4
+    true if @user_answer.length == 4 && !@user_answer.match( /[^0-9]/ )
+  end
+
+  def hint
+    msg = if @hints_count > 0
+      @hints_count -= 1
+      @hints.pop
+    else
+      'Hints are over'
+    end
+    @console.message(msg)
   end
 
   def compare_codes(user_code)
-    if secret_code == user_code
-      @console.message ('ebat ty krasavella')
-      return true
-    end
+    return true if secret_code == user_code
     result = ''
     uncatched_numbers = []
     @secret_code.each_index do |index|
@@ -50,6 +105,7 @@ class Game
 
     user_code.compact.each do |number|
       if uncatched_numbers.include?(number)
+        uncatched_numbers.delete(number)
         result << "-"
       end
     end
@@ -61,16 +117,8 @@ class Game
     @secret_code.empty? ? make_secret_code : @secret_code
   end
 
-  def win
-    @console.message 'Congrat. U did it'
-  end
-
-  def loose
-    @console.message 'What do we say God of Games? Not today'
-  end
-
   def one_more?
-    answer = @console.question 'one more?'
+    answer = @console.question 'one more? (y/n)'
     msg = if ['y', 'yes'].include? answer.downcase
       'nu go'
     else
@@ -82,10 +130,10 @@ class Game
   protected
   def make_secret_code
     4.times { @secret_code << rand(1..6) }
+    @hints = @secret_code.shuffle
     @secret_code
   end
 
 
 end
-Game.new
-# binding.pry
+game = Game.new
