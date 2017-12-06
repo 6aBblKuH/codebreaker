a1754541150bc58a70f5d532b5efd903b3ce889e
Dmitriy Bavykin <1996@3g.ua>
2017-12-06 19:57:38 +0200

    changing namespaces

--- lib/codebreaker/console.rb
+++ lib/codebreaker/console.rb
@@ -1,125 +1,129 @@
 # frozen_string_literal: true
 
-class Console
-  attr_reader :game, :difficulty_name
+module Codebreaker
 
-  def welcome
-    output(:welcome)
-    welcome_instructions
-  end
+  class Console
+    attr_reader :game, :difficulty_name
 
-  private
-
-  def welcome_instructions
-    case ask(:welcome_instruction).downcase
-    when 'game' then new_game
-    when 'stats' then output_statistics
-    when 'exit' then exit
-    else
-      output(:spelling_error)
+    def welcome
+      output(:welcome)
       welcome_instructions
     end
-  end
 
-  def new_game
-    rules
-    handle_difficulty
-    @game = Game.new(difficulty_name)
-    game_round
-  end
+    private
 
-  def game_round
-    while game.attempts.positive?
-      user_answer = round_question(game.attempts)
-      next hint if user_answer == 'hint'
-      return win if game.equal_codes?(user_answer)
-      round_message(user_answer)
+    def welcome_instructions
+      case ask(:welcome_instruction).downcase
+      when 'game' then new_game
+      when 'stats' then output_statistics
+      when 'exit' then exit
+      else
+        output(:spelling_error)
+        welcome_instructions
+      end
     end
-    lose
-  end
 
-  def handle_difficulty
-    @difficulty_name = ask(:choose_difficulty).to_sym
-    return if Game::DIFFICULTIES.key? @difficulty_name
-    output(:spelling_error)
-    handle_difficulty
-  end
+    def new_game
+      rules
+      handle_difficulty
+      @game = Codebreaker::Game.new(difficulty_name)
+      game_round
+    end
 
-  def hint
-    msg = game.hints.empty? ? :no_hint : game.take_a_hint!
-    output(msg)
-  end
+    def game_round
+      while game.attempts.positive?
+        user_answer = round_question(game.attempts)
+        next hint if user_answer == 'hint'
+        return win if game.equal_codes?(user_answer)
+        round_message(user_answer)
+      end
+      lose
+    end
 
-  def round_message(user_answer)
-    message = game.valid_answer?(user_answer) ? game.handle_guess(user_answer) : :invalid_number
-    output(message)
-  end
+    def handle_difficulty
+      @difficulty_name = ask(:choose_difficulty).to_sym
+      return if Game::DIFFICULTIES.key? @difficulty_name
+      output(:spelling_error)
+      handle_difficulty
+    end
 
-  def output(message)
-    puts message.is_a?(Symbol) ? phrases[message] : message
-  end
+    def hint
+      msg = game.hints.empty? ? :no_hint : game.take_a_hint!
+      output(msg)
+    end
 
-  def ask(question)
-    output(question) if question
-    gets.chomp
-  end
+    def round_message(user_answer)
+      message = game.valid_answer?(user_answer) ? game.handle_guess(user_answer) : :invalid_number
+      output(message)
+    end
 
-  def dichotomy_question?(question)
-    answer = ask(question).downcase
-    %w[yes y yeap].include?(answer)
-  end
+    def output(message)
+      puts message.is_a?(Symbol) ? phrases[message] : message
+    end
 
-  def rules
-    output(:rules)
-    difficulty_rules
-  end
+    def ask(question)
+      output(question) if question
+      gets.chomp
+    end
 
-  def output_statistics
-    handle_statistics_for_output.each { |record| output(record) }
-    welcome_instructions
-  end
+    def dichotomy_question?(question)
+      answer = ask(question).downcase
+      %w[yes y yeap].include?(answer)
+    end
 
-  def round_question(attempts)
-    ask("#{phrases[:round_question]}#{attempts}")
-  end
+    def rules
+      output(:rules)
+      difficulty_rules
+    end
 
-  def lose(secret_code)
-    output(phrases[:lose] << secret_code.join)
-  end
+    def output_statistics
+      handle_statistics_for_output.each { |record| output(record) }
+      welcome_instructions
+    end
 
-  def win
-    output(:win)
-    Loader.save_score(score_data) if dichotomy_question?(:save_score)
-    dichotomy_question?(:new_game) ? new_game : output(:goodbye)
-  end
+    def round_question(attempts)
+      ask("#{phrases[:round_question]}#{attempts}")
+    end
 
-  def lose
-    lose(game.secret_code)
-    dichotomy_question?(:new_game) ? new_game : output(:goodbye)
-  end
+    def lose(secret_code)
 
-  def difficulty_rules
-    Game::DIFFICULTIES.each do |diff_name, params|
-      output("#{diff_name}: #{params[:attempts]} attempts and #{params[:hints]} hints")
+    end
+
+    def win
+      output(:win)
+      Loader.save_score(score_data) if dichotomy_question?(:save_score)
+      dichotomy_question?(:new_game) ? new_game : output(:goodbye)
+    end
+
+    def lose
+      output(phrases[:lose] << game.secret_code.join)
+      dichotomy_question?(:new_game) ? new_game : output(:goodbye)
+    end
+
+    def difficulty_rules
+      Game::DIFFICULTIES.each do |diff_name, params|
+        output("#{diff_name}: #{params[:attempts]} attempts and #{params[:hints]} hints")
+      end
+    end
+
+    def phrases
+      @phrases ||= Loader.load_file('phrases')
+    end
+
+    def statistics
+      @statistics ||= Loader.load_file('statistics')
+    end
+
+    def handle_statistics_for_output
+      statistics.map do |record|
+        "#{record[:name]} won the game on #{record[:difficulty]} level and still had #{record[:attempts]} attempts and #{record[:hints]} hints"
+      end
+    end
+
+    def score_data
+      name = ask(:username)
+      { name: name, difficulty: @difficulty_name, attempts: game.attempts, hints: game.hints.size }
     end
   end
 
-  def phrases
-    @phrases ||= Loader.load_file('phrases')
-  end
-
-  def statistics
-    @statistics ||= Loader.load_file('statistics')
-  end
-
-  def handle_statistics_for_output
-    statistics.map do |record|
-      "#{record[:name]} won the game on #{record[:difficulty]} level and still had #{record[:attempts]} attempts and #{record[:hints]} hints"
-    end
-  end
-
-  def score_data
-    name = ask(:username)
-    { name: name, difficulty: @difficulty_name, attempts: game.attempts, hints: game.hints.size }
-  end
 end
--- lib/codebreaker/game.rb
+++ lib/codebreaker/game.rb
@@ -1,56 +1,60 @@
-# frozen_string_literal: true
+  # frozen_string_literal: true
+  
+module Codebreaker
 
-class Game
-  DIFFICULTIES = {
-    easy: { attempts: 30, hints: 3 },
-    medium: { attempts: 15, hints: 2 },
-    hard: { attempts: 10, hints: 1 }
-  }.freeze
+  class Game
+    DIFFICULTIES = {
+      easy: { attempts: 30, hints: 3 },
+      medium: { attempts: 15, hints: 2 },
+      hard: { attempts: 10, hints: 1 }
+    }.freeze
 
-  attr_reader :attempts, :hints, :secret_code
-  attr_accessor :user_code
+    attr_reader :attempts, :hints, :secret_code
+    attr_accessor :user_code
 
-  def initialize(difficulty)
-    @secret_code = Array.new(4) { rand(1..6) }
-    @attempts = DIFFICULTIES.dig(difficulty, :attempts)
-    @hints = secret_code.shuffle.take(DIFFICULTIES.dig(difficulty, :hints))
-  end
+    def initialize(difficulty)
+      @secret_code = Array.new(4) { rand(1..6) }
+      @attempts = DIFFICULTIES.dig(difficulty, :attempts)
+      @hints = secret_code.shuffle.take(DIFFICULTIES.dig(difficulty, :hints))
+    end
 
-  def handle_guess(user_answer)
-    @user_code = user_answer.each_char.map(&:to_i)
-    handle_numbers
-    @attempts -= 1
-    @round_result.empty? ? 'No matches' : @round_result
-  end
+    def handle_guess(user_answer)
+      @user_code = user_answer.each_char.map(&:to_i)
+      handle_numbers
+      @attempts -= 1
+      @round_result.empty? ? 'No matches' : @round_result
+    end
 
-  def valid_answer?(user_answer)
-    user_answer =~ /^[1-6]{4}$/
-  end
+    def valid_answer?(user_answer)
+      user_answer =~ /^[1-6]{4}$/
+    end
 
-  def take_a_hint!
-    @hints.pop
-  end
+    def take_a_hint!
+      @hints.pop
+    end
 
-  def equal_codes?(user_answer)
-    secret_code.join == user_answer
-  end
+    def equal_codes?(user_answer)
+      secret_code.join == user_answer
+    end
 
-  private
+    private
 
-  def check_numbers_for_correct_position
-    secret_code.map.with_index do |element, index|
-      next element unless element == user_code[index]
-      user_code[index] = nil
+    def check_numbers_for_correct_position
+      secret_code.map.with_index do |element, index|
+        next element unless element == user_code[index]
+        user_code[index] = nil
+      end
+    end
+
+    def handle_numbers
+      uncatched_numbers = check_numbers_for_correct_position
+      @round_result = '+' * uncatched_numbers.select(&:nil?).size
+      user_code.compact.map do |number|
+        next unless uncatched_numbers.compact.include?(number)
+        @round_result += '-'
+        uncatched_numbers[uncatched_numbers.index(number)] = nil
+      end
     end
   end
 
-  def handle_numbers
-    uncatched_numbers = check_numbers_for_correct_position
-    @round_result = '+' * uncatched_numbers.select(&:nil?).size
-    user_code.compact.map do |number|
-      next unless uncatched_numbers.compact.include?(number)
-      @round_result += '-'
-      uncatched_numbers[uncatched_numbers.index(number)] = nil
-    end
-  end
 end
